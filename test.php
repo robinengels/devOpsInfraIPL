41ea0d4c813e96e8a3f8d8323be196168cfc0934
Jacana <32939472+robinengels@users.noreply.github.com>
2020-10-26 12:23:06 +0100

    Add files via upload

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    
+    public function testInstanceCalculator()
+    {
+        $this->assertEquals("YES","YES");
+    }
+
+   
+ }
+ 
+ ?>
