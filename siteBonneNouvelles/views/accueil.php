47842bb25b4f80a37e6ad5e55fbd1767c001e77d
Jacana <32939472+robinengels@users.noreply.github.com>
2020-10-26 11:44:43 +0100

    Update accueil.php

--- siteBonneNouvelles/views/accueil.php
+++ siteBonneNouvelles/views/accueil.php
@@ -1,5 +1,5 @@
 		<section id="contenu">
 			<h2>Accueil</h2>
-			<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP selon une architecture didactique MVC OO.</p>
+			<p>Bienvenue aux séances d'exercices consacrées à RIENG un site Internet en PHP selon une architecture didactique MVC OO.</p>
 			<p>Jean-Luc Collinet.</p>
 		</section>
